424ce199c6bb7c67e62877dea33dc5fa09c4cb3c
Sophie Carboni <socarboni@gmail.com>
2020-12-23 10:10:48 +0100

    add automatisation of covered fun

---
+++ tools/extract-covered-fun.js
@@ -0,0 +1,18 @@
+var bass = require("../");
+var basslib = new bass();
+
+/////////////////////////PRETEST//////////////////////////////////
+
+basslib.EnableTags(true);
+basslib.EnableEncoder(true);
+basslib.EnableMixer(true);
+
+var libNames = basslib.WRAP_DEBUG_getAllLibNameActivated();
+
+for (let i in libNames) {
+  var { ffiFunDeclaration } = basslib.WRAP_DEBUG_getDebugData(libNames[i]);
+  console.log("From " + libNames[i] + " :");
+  for (let fun in ffiFunDeclaration) {
+    console.log("\t" + fun);
+  }
+}
