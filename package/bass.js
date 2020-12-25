4f4a5b68bb820196e925ae843dc808e5a2ea110f
Sophie Carboni <socarboni@gmail.com>
2020-12-25 12:32:14 +0100

    separating getPlatformDep

--- package/bass.js
+++ package/bass.js
@@ -24,11 +24,11 @@ util.inherits(Bass, EventEmitter);
 
 const path = require("path");
 const ffi = require("ffi-napi");
-const os = require("os");
 
 const fs = require("fs");
 
 const enableLibInt = require("./enableLibInt");
+const getPlatformDependencies = require("./getPlatformDependencies");
 
 // Read in the libs from this directory and import them
 var setters = [];
@@ -44,53 +44,6 @@ fs.readdirSync(path.join(".", "package", "libDeclarations")).forEach(function (
   if (file.indexOf(".js") > -1)
     libDeclarations.push(require("./" + path.join("libDeclarations", file)));
 });
-
-const libFile = require("./libFile");
-
-function getPlatformDependencies() {
-  let platform = os.platform();
-  let arch = os.arch();
-
-  switch (platform) {
-    case "win32":
-      const winLibFiles = {
-        bass: new libFile("bass", "bass.dll"),
-        mixer: new libFile("mixer", "bassmix.dll"),
-        encoder: new libFile("encoder", "bassenc.dll"),
-        tags: new libFile("tags", "tags.dll"),
-      };
-      if (arch == "x64") {
-        return { path: "win64", libFiles: winLibFiles };
-      } else if (arch == "x86") {
-        return { path: "win32", libFiles: winLibFiles };
-      } else {
-        return null;
-      }
-    case "darwin":
-      const macosLibFiles = {
-        bass: new libFile("bass", "libbass.dylib"),
-        mixer: new libFile("mixer", "libbassmix.dylib"),
-        encoder: new libFile("encoder", "libbassenc.dylib"),
-        tags: new libFile("tags", "libtags.dylib"),
-      };
-      return { path: "macOs", libFiles: macosLibFiles };
-    case "linux":
-      const linuxLibFiles = {
-        bass: new libFile("bass", "libbass.so"),
-        mixer: new libFile("mixer", "libbassmix.so"),
-        encoder: new libFile("encoder", "libbassenc.so"),
-        tags: new libFile("tags", "libtags.so"),
-      };
-      if (arch == "x64") {
-        return { path: "linux64", libFiles: linuxLibFiles };
-      } else if (arch == "x86") {
-        return { path: "linux32", libFiles: linuxLibFiles };
-      } else {
-        return null;
-      }
-  }
-  return null;
-}
 
 function Bass(options) {
   if (!options) options = {};
---
+++ package/getPlatformDependencies.js
@@ -0,0 +1,53 @@
+/**
+ * Created by scarboni on 25.12.2020
+ */
+
+const os = require("os");
+const libFile = require("./libFile");
+
+function getPlatformDependencies() {
+  let platform = os.platform();
+  let arch = os.arch();
+
+  switch (platform) {
+    case "win32":
+      const winLibFiles = {
+        bass: new libFile("bass", "bass.dll"),
+        mixer: new libFile("mixer", "bassmix.dll"),
+        encoder: new libFile("encoder", "bassenc.dll"),
+        tags: new libFile("tags", "tags.dll"),
+      };
+      if (arch == "x64") {
+        return { path: "win64", libFiles: winLibFiles };
+      } else if (arch == "x86") {
+        return { path: "win32", libFiles: winLibFiles };
+      } else {
+        return null;
+      }
+    case "darwin":
+      const macosLibFiles = {
+        bass: new libFile("bass", "libbass.dylib"),
+        mixer: new libFile("mixer", "libbassmix.dylib"),
+        encoder: new libFile("encoder", "libbassenc.dylib"),
+        tags: new libFile("tags", "libtags.dylib"),
+      };
+      return { path: "macOs", libFiles: macosLibFiles };
+    case "linux":
+      const linuxLibFiles = {
+        bass: new libFile("bass", "libbass.so"),
+        mixer: new libFile("mixer", "libbassmix.so"),
+        encoder: new libFile("encoder", "libbassenc.so"),
+        tags: new libFile("tags", "libtags.so"),
+      };
+      if (arch == "x64") {
+        return { path: "linux64", libFiles: linuxLibFiles };
+      } else if (arch == "x86") {
+        return { path: "linux32", libFiles: linuxLibFiles };
+      } else {
+        return null;
+      }
+  }
+  return null;
+}
+
+exports = module.exports = getPlatformDependencies;
