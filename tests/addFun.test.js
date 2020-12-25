9c538a5e2e1161f17741468ce23d2b7d6da5a98f
Sophie Carboni <socarboni@gmail.com>
2020-12-25 12:41:59 +0100

    preparing to mix light and basic package

---
+++ tests/addFun.test.js
@@ -0,0 +1,29 @@
+var getBass = require("../tools/getBass");
+var basslib = getBass({
+  silent: true,
+  ffiFunDeclaration: { bass: {}, tags: {} },
+});
+
+basslib.EnableTags(true);
+basslib.EnableEncoder(true);
+basslib.EnableMixer(true);
+
+test("test basic linking", () => {
+  var libNames = basslib.WRAP_DEBUG_getAllLibNameActivated();
+  var linked = 0;
+  var awaited = 0;
+  for (let i in libNames) {
+    var { ffiFunDeclaration } = basslib.WRAP_DEBUG_getDebugData(libNames[i]);
+    awaited += Object.keys(ffiFunDeclaration).length;
+  }
+
+  for (let i in libNames) {
+    var { ffiFunDeclaration } = basslib.WRAP_DEBUG_getDebugData(libNames[i]);
+    for (let fun in ffiFunDeclaration) {
+      // console.log(fun + " return " + basslib[fun]());
+      basslib[fun]();
+      linked++;
+    }
+  }
+  expect(awaited).toBe(linked);
+});
---
+++ tools/getBass.js
@@ -0,0 +1,6 @@
+function getBass(options) {
+  var bass = require("../");
+  return new bass(options);
+}
+
+exports = module.exports = getBass;
