b8ccb5356e439828836d5842896e67c6ec0465d6
Sophie Carboni <socarboni@gmail.com>
2021-01-01 11:01:38 +0100

    delete abandonned exampels

--- examples/add-features.js
+++
@@ -1,9 +0,0 @@
-var getBass = require("../tools/getBass");
-var basslib = getBass({
-  silent: true,
-});
-
-const ffiFunDeclaration = {
-  BASS_GetVersion: ["int", []],
-};
-var basslib = new Bass({ ffiFunDeclaration: { bass: ffiFunDeclaration } });
